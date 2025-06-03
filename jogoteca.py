2809177e59c5dce335e428aad80a7112edb2927d
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2025-06-03 17:18:17 -0300

    feat: Refactor game list rendering in 'jogoteca.py' and 'lista.html' for improved structure and clarity.

--- jogoteca.py
+++ jogoteca.py
@@ -1,13 +1,28 @@
 from flask import Flask, render_template
 
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+    def __str__(self):
+        return f'{self.nome} - {self.categoria} - {self.console}'
+
+
 app = Flask(__name__)
 
 
 @app.route('/inicio')
 def ola():
-    listaJogos = ['The Legend of Zelda',
-                  'Super Mario Bros', 'Metroid', 'Donkey']
-    return render_template('Lista.html', titulo='Jogos', jogos=listaJogos)
+    jogo1 = Jogo('The Legend of Zelda', 'Aventura', 'NES')
+    jogo2 = Jogo('Super Mario Bros', 'Plataforma', 'NES')
+    jogo3 = Jogo('Metroid', 'Aventura', 'NES')
+
+    listaJogos = [jogo1, jogo2, jogo3]
+
+    return render_template('lista.html', titulo='Jogos', jogos=listaJogos)
 
 
 app.run()
