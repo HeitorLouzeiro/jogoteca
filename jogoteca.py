0f09cad9fafd1f3bcf4b2eddc1db591858af4d25
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2025-06-03 17:37:18 -0300

    feat: Add 'novo.html' template for new game submission and refactor routes in 'jogoteca.py'.

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 class Jogo:
@@ -7,22 +7,33 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
-    def __str__(self):
-        return f'{self.nome} - {self.categoria} - {self.console}'
 
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Hack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista = [jogo1, jogo2, jogo3]
 
 app = Flask(__name__)
 
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo('The Legend of Zelda', 'Aventura', 'NES')
-    jogo2 = Jogo('Super Mario Bros', 'Plataforma', 'NES')
-    jogo3 = Jogo('Metroid', 'Aventura', 'NES')
-
-    listaJogos = [jogo1, jogo2, jogo3]
-
-    return render_template('lista.html', titulo='Jogos', jogos=listaJogos)
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
-app.run()
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogo', jogos=lista)
+
+
+app.run(debug=True)
